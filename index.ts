25e0ee1af5a3fd1d4befd4668aced017c1b51a70
Sergyo97 <31071190+Sergyo97@users.noreply.github.com>
2023-01-17 21:43:46 -0500

    Initial commit

---
+++ index.ts
@@ -0,0 +1,19 @@
+import { from } from 'rxjs';
+
+from(['Alice', 'Ben', 'Charlie']).subscribe({
+  next: (value) => console.log(value),
+  complete: () => console.log('Completed'),
+});
+
+const somePromise = new Promise((resolve, reject) => {
+  //resolve('Resolve!');
+  reject('Rejected');
+});
+
+const observableFromPromise$ = from(somePromise);
+
+observableFromPromise$.subscribe({
+  next: (value) => console.log(value),
+  error: (err) => console.log('Error', err),
+  complete: () => console.log('Completed'),
+});
